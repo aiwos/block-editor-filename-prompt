ab10f39c168d2ec68801d7051f659409cb3cb280
Jurriaan Koops <jurriaankoops@gmail.com>
2026-02-02 16:17:34 +0100

    functional improvements

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-plugins'), 'version' => '07ce5b92f7d7b81eee26');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-plugins'), 'version' => 'c9afad19b58823fd64fe');
