01a2caa1c9e3a8683624f0598a2748f459d7adcd
JurriaanK <jurriaankoops@gmail.com>
2026-02-06 10:36:05 +0100

    build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-plugins'), 'version' => 'f0cbc98007c0e0b325f9');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-plugins'), 'version' => '16ee1724a636791ecaf5');
